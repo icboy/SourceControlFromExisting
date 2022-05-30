9f32d20a79e6940e13d3b545d8acb992a5dabe21
imac <icboy@msn.com>
2022-05-30 21:49:22 +0800

    Initial Commit

---
+++ SourceControlFromExisting/SourceControlFromExistingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlFromExistingApp.swift
+//  SourceControlFromExisting
+//
+//  Created by imac on 2022/5/30.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlFromExistingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
